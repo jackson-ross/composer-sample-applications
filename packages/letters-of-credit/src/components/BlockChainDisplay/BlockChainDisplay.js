5575e44df7e47059eee45bb8377d7f02fe03e61d
erin-hughes <35696678+erin-hughes@users.noreply.github.com>
2018-05-04 12:24:13 +0100

    Added more specific info to the blockchain display (#34)
    Signed-off-by: Erin Hughes <Erin.Hughes@ibm.com>

--- packages/letters-of-credit/src/components/BlockChainDisplay/BlockChainDisplay.js
+++ packages/letters-of-credit/src/components/BlockChainDisplay/BlockChainDisplay.js
@@ -10,18 +10,58 @@ class BlockChainDisplay extends React.Component {
     }
   }
 
+  getSpecificInfo() {
+    let transactions = [];
+    this.props.transactions.map((tx, index) => {
+      let transaction = {
+        name: "",
+        timestamp: tx.transactionTimestamp
+      }
+      if (tx.transactionType === 'org.acme.loc.InitialApplication') {
+        transaction.name = 'Created by Alice';
+      } else if (tx.transactionType === 'org.acme.loc.Approve') {
+        switch (index) {
+          case 1: 
+            transaction.name = 'Approved by Matías'; 
+            break;
+          case 2:
+            transaction.name = 'Approved by Ella';
+            break;
+          case 3:
+            transaction.name = 'Approved by Bob';
+            break;
+          default: 
+            transaction.name = 'Approved';
+            break;
+        }
+      } else if (tx.transactionType === 'org.acme.loc.ShipProduct') {
+        transaction.name = 'Shipped by Bob';
+      } else if (tx.transactionType === 'org.acme.loc.ReceiveProduct') {
+        transaction.name = 'Received by Alice';
+      } else if (tx.transactionType === 'org.acme.loc.ReadyForPayment') {
+        transaction.name = 'Paid by Matías';
+      } else if (tx.transactionType === 'org.acme.loc.Close') {
+        transaction.name = 'Closed by Ella';
+      } else {
+        transaction.name = 'Rejected';
+      }
+      transactions.push(transaction);
+    });
+    return transactions;
+  }
+
   addLeadingZero(number) {
     return (number < 10) ? "0" + number : number;
   }
 
   render() {
-    let transactions = this.props.transactions;
+    let transactions = this.getSpecificInfo();
     let blocks = [];
     if(transactions.length) {
       for (let i = transactions.length; i > 0; i--) {
-        let name = transactions[i-1].transactionType.split(".")[3];
+        let name = transactions[i-1].name;
         let blockNumber = this.addLeadingZero(i);
-        let dateTime = new Date(transactions[i-1].transactionTimestamp);
+        let dateTime = new Date(transactions[i-1].timestamp);
         let date = dateTime.getFullYear() + '-' + this.addLeadingZero(dateTime.getMonth()+1) + '-' + this.addLeadingZero(dateTime.getDate());
         let time = dateTime.toTimeString().split(' ')[0];
         blocks.push(<Block transactionDetails={name} date={date} time={time} number={blockNumber}/>);
